1a617ee3639a659d67f27a72038a7725d9c9a379
faddiDev <faddi.dev@gmail.com>
2020-11-26 20:17:11 +0700

    add multiple div

--- fd-carousel.js
+++ fd-carousel.js
@@ -1,24 +1,25 @@
 for(var jum=0;jum<document.getElementsByClassName("fd-carousel").length;jum++) {
+	carousels(0, jum);
+}
+
+function carousels(i, jum) {
 	var carousel = document.getElementsByClassName("fd-carousel")[jum];
 	var img = document.createElement("img");
 	
 	var width = carousel.getAttribute("width-carousel");
 	var times = carousel.getAttribute("times-carousel");
 	var src = carousel.getAttribute("src-carousel").split(";");
-	
-	function carousels(i) {
-		if(carousel.hasChildNodes()) {
-			carousel.removeChild(carousel.childNodes[0]);
-		}
-		img.setAttribute("src", src[i]);
-		img.setAttribute("max-width", "100%");
-		img.setAttribute("width", width);
-		img.setAttribute("height", "auto");
-		carousel.appendChild(img);
-		setTimeout(function(){
-			i = (i<src.length-1) ? i+1 : 0;
-			carousels(i);
-		}, 2000);
+
+	if(carousel.hasChildNodes()) {
+		carousel.removeChild(carousel.childNodes[0]);
 	}
-	carousels(0);
+	img.setAttribute("src", src[i]);
+	img.setAttribute("max-width", "100%");
+	img.setAttribute("width", width);
+	img.setAttribute("height", "auto");
+	carousel.appendChild(img);
+	setTimeout(function(){
+		i = (i<src.length-1) ? i+1 : 0;
+		carousels(i, jum);
+	}, 2000);
 }
