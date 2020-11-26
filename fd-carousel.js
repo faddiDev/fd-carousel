46ea6d2a31c3399ad96b7863ee7ddccda9913515
faddiDev <faddi.dev@gmail.com>
2020-11-26 20:36:25 +0700

    update carousel

--- fd-carousel.js
+++ fd-carousel.js
@@ -6,20 +6,18 @@ function carousels(i, jum) {
 	var carousel = document.getElementsByClassName("fd-carousel")[jum];
 	var img = document.createElement("img");
 	
-	var width = carousel.getAttribute("width-carousel");
 	var times = carousel.getAttribute("times-carousel");
 	var src = carousel.getAttribute("src-carousel").split(";");
 
 	if(carousel.hasChildNodes()) {
 		carousel.removeChild(carousel.childNodes[0]);
 	}
+	
 	img.setAttribute("src", src[i]);
-	img.setAttribute("max-width", "100%");
-	img.setAttribute("width", width);
-	img.setAttribute("height", "auto");
+	img.style.cssText = "display:block;margin-left:auto;margin-right:auto;height:100%;";
 	carousel.appendChild(img);
 	setTimeout(function(){
 		i = (i<src.length-1) ? i+1 : 0;
 		carousels(i, jum);
-	}, 2000);
+	}, parseInt(times));
 }
